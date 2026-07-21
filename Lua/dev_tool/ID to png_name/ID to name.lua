91741b9a07c564c7e17b06c47ea3579b766745b8
AutoPotato <a0905361233@gmail.com>
2026-07-22 01:09:50 +0800

    Last commit before I go to sleep, basically finished the ID list generator, now the next step would be using the list to put all the items in-game so i can see and test their sprites.

--- Lua/dev_tool/ID to png_name/ID to name.lua
+++ Lua/dev_tool/ID to png_name/ID to name.lua
@@ -1,10 +1,44 @@
 local xml2lua = require("xml2lua")
 local handler = require("xmlhandler.tree")
 local Files = {
-    [[D:\Developer Project\EA and Anime Patch - PRIVATEER GROUP\Example\xml\EA armor.xml]],
-    [[D:\Developer Project\EA and Anime Patch - PRIVATEER GROUP\Example\xml\EAFFE Enhanced_Equipment.xml]],
-    [[D:\Developer Project\EA and Anime Patch - PRIVATEER GROUP\Example\xml\EAF juggernaut.xml]],
-    [[D:\Developer Project\EA and Anime Patch - PRIVATEER GROUP\Example\xml\EAF lagacy armor.xml]],
+    [[.\xml\EA armor.xml]],
+    [[.\xml\EAFFE Enhanced_Equipment.xml]],
+    [[.\xml\EAF juggernaut.xml]],
+    [[.\xml\EAF lagacy armor.xml]],
+    -- [[.\animeArmor.xml]],
+}
+
+local exceptions = {
+    'scp_simplehelmet',
+    'scp_combathelmet',
+    'scp_heavycombathelmet',
+    'scp_renegadeheavyhelmet',
+    'scp_renegadecaptainhat',
+    'scp_assaultpack',
+    'scp_heavypack',
+    'scp_tacpack',
+    'scp_fieldpack',
+    'scp_protopack',
+    'scp_renegadehelmet',
+    'scp_yuihelmet',
+    'scp_livhelmet',
+    'scp_veronhelmet',
+    'scp_hardeneddivingmask',
+    'scp_clownbatmask',
+    'scp_marauderhelmet',
+    'sgt_longrangeradio',
+    'sgt_bobag',
+    'sgt_fieldcap',
+    'sgt_cowboy',
+    'sgt_blackopshelm',
+    'sgt_blackopshelm2',
+    'sgt_blackopshelm3',
+    'sgt_blackopshelm4',
+    'sgt_tachelm',
+    'sgt_medhelm',
+    'eaf_juggernauthelmet',
+    'scp_livhelmet_old',
+    'scp_veronhelmet_old',
 }
 
 local function removeDuplicates(arr)
@@ -22,11 +56,12 @@ end
 local function dump_tb(o)
     if type(o) == 'table' then
         for i, element in pairs(o) do
-            print(element)
+            io.write(element)
         end
     else
       return tostring(o)
     end
+    print()
 end
 
 local function getItemID(filePath)
@@ -43,23 +78,32 @@ local function getItemID(filePath)
     local parser = xml2lua.parser(listHandler) --tell the parser to use listHandler to store the results
     parser:parse(EAmodXML) --actually parses the data then put it in listHandler
     local item = listHandler.root.Items.Item
-
     local rawListOutput = {}
+
+    for i in ipairs(item) do
+        for _2, v2 in pairs(exceptions) do
+            if item[i]._attr.identifier == v2 then
+                -- print('REMOVED '.. item[i]._attr.identifier)
+                table.remove(item, i)
+            end
+        end
+    end
 
     for item_Index in ipairs(item) do
         table.insert(rawListOutput, item[item_Index]._attr.identifier)
 
         for sprite_index in ipairs(item[item_Index].Wearable.sprite) do
-            table.insert(rawListOutput, "    " .. item[item_Index].Wearable.sprite[sprite_index]._attr.texture)
+            table.insert(rawListOutput, ";" .. item[item_Index].Wearable.sprite[sprite_index]._attr.texture)
         end
 
         if not item[item_Index].Wearable.sprite[1] then
-            table.insert(rawListOutput, "    " .. item[item_Index].Wearable.sprite._attr.texture)
+            table.insert(rawListOutput, ";" .. item[item_Index].Wearable.sprite._attr.texture)
         end
-
+        
         rawListOutput = removeDuplicates(rawListOutput)
         dump_tb(rawListOutput)
         rawListOutput = {}
+
     end
 end
 
