eb66afd8dc53a44d7bc0a80a5da6959dc40672de
AutoPotato <a0905361233@gmail.com>
2026-07-24 01:06:07 +0800

    Privateer commission (And restructure this repo to contain all the current and future mods that i might do)
    * First Commit
    
    * added Jobgear folder to be staged change
    
    * added all the files
    
    * Rewrote the code
    
    * Last commit before I go to sleep, basically finished the ID list generator, now the next step would be using the list to put all the items in-game so i can see and test their sprites.
    
    * little variable name change
    
    * Resructured the folders and be ready to merge into main branch, so I can stop working on just branches, that github doesn't track contribution.

---
+++ EA and Anime Patch - PRIVATEER GROUP/Lua/dev_tool/ID to png_name/ID to name.lua
@@ -0,0 +1,117 @@
+local xml2lua = require("xml2lua")
+local handler = require("xmlhandler.tree")
+local Files = {
+    [[.\xml\EA armor.xml]],
+    [[.\xml\EAFFE Enhanced_Equipment.xml]],
+    [[.\xml\EAF juggernaut.xml]],
+    [[.\xml\EAF lagacy armor.xml]],
+    -- [[.\animeArmor.xml]],
+}
+
+local exceptions = {
+    'scp_simplehelmet',
+    'scp_combathelmet',
+    'scp_heavycombathelmet',
+    'scp_renegadeheavyhelmet',
+    'scp_renegadecaptainhat',
+    'scp_assaultpack',
+    'scp_heavypack',
+    'scp_tacpack',
+    'scp_fieldpack',
+    'scp_protopack',
+    'scp_renegadehelmet',
+    'scp_yuihelmet',
+    'scp_livhelmet',
+    'scp_veronhelmet',
+    'scp_hardeneddivingmask',
+    'scp_clownbatmask',
+    'scp_marauderhelmet',
+    'sgt_longrangeradio',
+    'sgt_bobag',
+    'sgt_fieldcap',
+    'sgt_cowboy',
+    'sgt_blackopshelm',
+    'sgt_blackopshelm2',
+    'sgt_blackopshelm3',
+    'sgt_blackopshelm4',
+    'sgt_tachelm',
+    'sgt_medhelm',
+    'eaf_juggernauthelmet',
+    'scp_livhelmet_old',
+    'scp_veronhelmet_old',
+}
+
+local function removeDuplicates(arr)
+    local hash = {}
+    local res = {}
+    for _, v in ipairs(arr) do
+        if not hash[v] then
+            hash[v] = true
+            table.insert(res, v)
+        end
+    end
+    return res
+end
+
+local function dump_tb(o)
+    if type(o) == 'table' then
+        for i, element in pairs(o) do
+            io.write(element)
+        end
+    else
+      return tostring(o)
+    end
+    print()
+end
+
+local function getItemID(filePath)
+    local EAmodXmlFile = io.open(filePath, "r")
+    if EAmodXmlFile == nil then 
+        return   
+    end
+    EAmodXmlFile:read("*l")
+    local EAmodXML = EAmodXmlFile:read("*a")
+    io.close(EAmodXmlFile)
+    
+    --Instantiates the XML parser
+    local listHandler = handler:new() --create a new handler to store xml data
+    local parser = xml2lua.parser(listHandler) --tell the parser to use listHandler to store the results
+    parser:parse(EAmodXML) --actually parses the data then put it in listHandler
+    local item = listHandler.root.Items.Item
+    local rawListOutput = {}
+
+    for i in ipairs(item) do
+        for _, v in pairs(exceptions) do
+            if item[i]._attr.identifier == v then
+                -- print('REMOVED '.. item[i]._attr.identifier)
+                table.remove(item, i)
+            end
+        end
+    end
+
+    for item_Index in ipairs(item) do
+        table.insert(rawListOutput, item[item_Index]._attr.identifier)
+
+        for sprite_index in ipairs(item[item_Index].Wearable.sprite) do
+            table.insert(rawListOutput, ";" .. item[item_Index].Wearable.sprite[sprite_index]._attr.texture)
+        end
+
+        if not item[item_Index].Wearable.sprite[1] then
+            table.insert(rawListOutput, ";" .. item[item_Index].Wearable.sprite._attr.texture)
+        end
+        
+        rawListOutput = removeDuplicates(rawListOutput)
+        dump_tb(rawListOutput)
+        rawListOutput = {}
+
+    end
+end
+
+local function printwithIndex()
+    for files_Index in ipairs(Files) do
+        getItemID(Files[files_Index])
+        print("")
+    end
+end
+
+printwithIndex()
---
+++ Enhanced Armaments and Animated Barotrauma Patch/Lua/Autorun/retexture.lua
@@ -0,0 +1,109 @@
+local textureRegistry = {
+    -- ["itemID"]             = "spriteFileName",
+    ["scp_heavyvest"]                  = "Anime_heavyarmor.png",
+    ["scp_heavyrig"]                   = "Anime_heavyarmorrig.png",
+    ["scp_specialrig"]                 = "Anime_specialrig.png",
+    ["scp_renegadespecialrig"]         = "Anime_renegadespecialrig.png",
+    ["scp_renegadevest"]               = "Anime_renegadevest.png",
+    ["scp_heavyuniform"]               = "Anime_heavyuniform.png",
+    ["scp_heavyrenuniform"]            = "Anime_heavyrenuniform.png",
+    ["scp_renegadeplatecarrier"]       = "Anime_renegadeplatecarrier.png",
+    ["scp_heavyhazmatuniform"]         = "Anime_rencbrnsuit.png",
+    ["scp_liquidatorsuit"]             = "Anime_highperfhazmat.png",
+    ["scp_combatmedicuniform"]         = "Anime_combatmedic.png",
+    ["scp_renegadecombatmedicuniform"] = "Anime_combatmedicrenegade.png",
+    ["scp_renegadeuniform"]            = "Anime_renegadeuniform.png",
+    ["scp_renegadedivingsuit"]         = "Anime_renegadesuit.png",
+    ["scp_renegadecaptainuniform"]     = "Anime_renegadecaptain.png",
+    ["sgt_coalitioncaptainuniform"]    = "Anime_coalitioncaptain.png",
+    ["scp_combathardsuit"]             = "Anime_combathardsuit.png",
+    ["scp_softvest"]                   = "Anime_softvest.png",
+    ["scp_riotvest"]                   = "Anime_riotvest.png",
+    ["sgt_securityvest"]               = "Anime_riotsuit.png",
+    ["scp_yuirig"]                     = "Anime_specopsvest.png",
+    ["scp_specopsuniform"]             = "Anime_specopsuniform.png",
+    ["scp_livuniform"]                 = "Anime_vanguarduniform.png",
+    ["scp_livrig"]                     = "Anime_vanguardrig.png",
+    ["scp_veronrig"]                   = "Anime_protectrig.png",
+    ["scp_marauderrig"]                = "Anime_marauderrig.png",
+    ["scp_marauderuniform"]            = "Anime_marauderuniform.png",
+    ["scp_veronuniform"]               = "Anime_protectuniform.png",
+    ["scp_lightuniform"]               = "Anime_lightuniform.png",
+    ["scp_cbrnsuit"]                   = "Anime_cbrnsuit.png",
+    ["scp_clownbatuniform"]            = "Anime_clowncombatdress.png",
+    ["scp_honkmasteroutfit"]           = "Anime_clowncombatdress2.png",
+    ["scp_interceptorsuit"]            = "Anime_interceptsuit.png",
+    ["scp_sevasuit"]                   = "Anime_apsseva.png",
+    ["sgt_banditclothes1"]             = "Anime_bandit_1.png",
+}
+
+local exceptions = {
+    --skip items that have a different texture path.
+    "Heavy Security Vest Holster",
+    "highperfhazmat helm Wearable",
+    "intercepsuit Suit Helmet Wearable",
+    "combathardsuithelmet",
+    "Renegade Diving Suit Helmet Wearable",
+}
+
+local function is_exception(spriteName)
+    for i, exception in pairs(exceptions) do
+        if spriteName == exception then
+            return true
+        end
+    end
+    return false
+end
+
+local function InjectAnimeEASprites()
+    local totalItemsProcessed = 0
+    --Loop through every pair of data.
+    for itemID, spriteFileName in pairs(textureRegistry) do
+            -- look for Item ID and get the raw data.
+        local customItemPrefab = ItemPrefab.GetItemPrefab(itemID)
+        if not customItemPrefab then
+            print("[Anime EA Mod] Error: Could not find the item's ID(identifier).")
+        else
+            --Get the xml layout.
+            local config = customItemPrefab.ConfigElement
+            if not config then 
+                print("[Anime EA Mod] Error: ConfigElement is empty.")
+                return
+            end
+            
+            -- Get <Wearable>'s every element, once found, break.
+            local wearableFind = nil
+            for i in config.Elements() do
+                if i.Name.ToString() == "Wearable" then
+                    wearableFind = i
+                    break
+                end
+            end
+            
+            if not wearableFind then
+                print("[Anime EA Mod] Warning: No <Wearable> component found in this item.")
+                return
+            -- Found the wearable, now trying to replace the texture.   
+            end
+            local count = 0
+
+            for elementsInside in wearableFind.Elements() do
+                if elementsInside.Name.ToString() == "sprite" and not is_exception(elementsInside.GetAttributeString("name", nil)) then
+                    -- Direct the texture's path to this mod.
+                    elementsInside.SetAttributeValue("texture", "%ModDir:3251262845%/Jobgear/" ..  spriteFileName)
+                    count = count + 1
+                end
+            end
+
+            wearableFind.Elements()
+            totalItemsProcessed = totalItemsProcessed + 1
+            --print("[Anime EA Mod]" .. itemID .. " | " .. spriteFileName.. " Successfully injected textures into " .. count .. " limbs!")
+        end
+    end
+    print("[Anime Enhanced Armaments Patch Mod] Done. Total items retextured: " .. totalItemsProcessed)
+end
+
+print("[Anime Enhanced Armaments Patch Mod]====Beginning====")
+print("[Anime Enhanced Armaments Patch Mod] Replacing EA Textures with the Anime version.")
+InjectAnimeEASprites()
+
